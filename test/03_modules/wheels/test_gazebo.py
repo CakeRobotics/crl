127cde3c0787565199e7bd4c855ccb0a60214945
Mostafa Farzan <farzan@cakerobotics.com>
2021-10-06 00:16:31 +0330

    Un-ignore wheels tests

---
+++ test/03_modules/wheels/test_gazebo.py
@@ -0,0 +1,77 @@
+import asyncio
+
+import cake
+from cake.modules.hardware.wheels.WheelsGazebo import WheelsGazebo
+from geometry_msgs.msg import Twist
+
+
+def test_init():
+    props = {
+        'name': 'name',
+        'hardware': {
+            'wheels-1': {
+                'type': 'wheels',
+                'sim': True,
+            }
+        }
+    }
+    robot = cake.Robot(props)
+    assert isinstance(robot.wheels, WheelsGazebo)
+    robot.wheels.set_speed(2)  # type: ignore
+    robot.shutdown()
+
+
+def test_set_speed():
+    props = {
+        'name': 'name',
+        'hardware': {
+            'wheels-1': {
+                'type': 'wheels',
+                'sim': True,
+            }
+        }
+    }
+    robot = cake.Robot(props)
+    @robot.runtime.run_in_event_loop
+    async def body():
+        def callback(msg: Twist):
+            global posted_target_speed
+            posted_target_speed = msg.linear.x
+        robot.runtime.ros_interface.create_subscription(
+            Twist, '/cake/wheels/vel_cmd', callback, 10
+        )
+        await robot.wheels.set_speed(2)
+        await asyncio.sleep(1e-3)
+        robot.runtime.assert_ros_interface_ok()
+        assert posted_target_speed == 2
+
+    body()  # type: ignore
+    robot.shutdown()
+
+
+def test_set_rotation_rate():
+    props = {
+        'name': 'name',
+        'hardware': {
+            'wheels-1': {
+                'type': 'wheels',
+                'sim': True,
+            }
+        }
+    }
+    robot = cake.Robot(props)
+    @robot.runtime.run_in_event_loop
+    async def body():
+        def callback(msg: Twist):
+            global posted_target_speed
+            posted_target_speed = msg.angular.z
+        robot.runtime.ros_interface.create_subscription(
+            Twist, '/cake/wheels/vel_cmd', callback, 10
+        )
+        await robot.wheels.set_rotation_rate(3)
+        await asyncio.sleep(1e-3)
+        robot.runtime.assert_ros_interface_ok()
+        assert posted_target_speed == 3
+
+    body()  # type: ignore
+    robot.shutdown()
---
+++ test/03_modules/wheels/test_params.py
@@ -0,0 +1,51 @@
+import pytest
+import cake
+
+
+def test_uninitialized():
+    robot = cake.Robot()
+    with pytest.raises(cake.exceptions.UndefinedHardware):
+        robot.wheels.set_speed(2)  # type: ignore
+    robot.shutdown()
+
+
+def test_initialized_without_wheels():
+    props = {
+        'name': 'name',
+        'hardware': {}
+    }
+    robot = cake.Robot(props)
+    with pytest.raises(cake.exceptions.UndefinedHardware):
+        robot.wheels.set_speed(2)  # type: ignore
+    robot.shutdown()
+
+
+def test_initialized_with_too_many_wheels():
+    props = {
+        'name': 'name',
+        'hardware': {
+            'wheels-1': {
+                'type': 'wheels',
+            },
+            'wheels-2': {
+                'type': 'wheels',
+            }
+        }
+    }
+    with pytest.raises(Exception, match='Only 1 set of wheels is currently supported.'):
+        cake.Robot(props)
+
+
+def test_initialized():
+    props = {
+        'name': 'name',
+        'hardware': {
+            'wheels-1': {
+                'type': 'wheels',
+                'dummy': True,
+            }
+        }
+    }
+    robot = cake.Robot(props)
+    robot.wheels.set_speed(2)  # type: ignore
+    robot.shutdown()
