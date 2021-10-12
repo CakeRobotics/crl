30574771e140344366d619f9285b624830781439
Mostafa Farzan <farzan@cakerobotics.com>
2021-10-13 02:52:08 +0330

    Limit ROS spin rate to 10 kHz
    Reduces CPU usage (and therefore power uage)

--- src/cake/ros/interface.py
+++ src/cake/ros/interface.py
@@ -9,14 +9,14 @@ class RosInterface:
 
     def __del__(self):
         self.shutdown()
-    
+
     def shutdown(self):
         rclpy.shutdown()
 
     async def spin_coro(self):
         while rclpy.ok():
             rclpy.spin_once(self.__node__, timeout_sec=0)
-            await asyncio.sleep(0)
+            await asyncio.sleep(1e-4)
 
     def create_publisher(self, *args, thread_check=True):
         if thread_check and not self.runtime.am_i_running_in_event_loop_thread():
