1adb2a430ea6a82abdfbdbd9c09f7086dbe818a5
Mostafa Farzan <farzan@cakerobotics.com>
2021-10-26 20:53:56 +0330

    Improve example

--- examples/sway/main.py
+++ examples/sway/main.py
@@ -7,8 +7,10 @@ import cake
 def main():
     robot = cake.Robot()
     while True:
+        print("Going forward")
         robot.wheels.set_speed(0.5)
         time.sleep(4)
+        print("Going back")
         robot.wheels.set_speed(-0.5)
         time.sleep(4)
 
