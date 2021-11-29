2e578ff4acb40e93c01478edd733f0494d179526
Mostafa Farzan <farzan@cakerobotics.com>
2021-11-29 18:12:43 +0330

    Add test for backend_loop exceptions

---
+++ test/02_runtime/test_05_exceptions.py
@@ -0,0 +1,12 @@
+import pytest
+from cake import Robot
+
+def test_exceptions_from_backend_loop():
+    robot = Robot({})
+    assert robot.health()
+    @robot.runtime.run_in_event_loop
+    async def body():
+        raise Exception("test-exception")
+    with pytest.raises(Exception, match='test-exception'):
+        body()  # type: ignore
+    robot.shutdown()
