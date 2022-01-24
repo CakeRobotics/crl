9c83944d989d96a97df142386fddd96f2815af9b
Mostafa Farzan <farzan@cakerobotics.com>
2022-01-24 04:23:01 +0330

    Fix bundler tests

--- test/04_bundler/test_get_dockerfile.py
+++ test/04_bundler/test_get_dockerfile.py
@@ -8,12 +8,12 @@ def test_get_dockerfile():
     expected_lines = \
 """FROM ros:galactic-ros-base-focal
 RUN apt-get update
-RUN apt-get install -y python3-pip
 RUN pip3 install git+https://github.com/CakeRobotics/crl
 RUN pip3 install numpy scipy
 COPY . /app
 CMD ["python3", "-u", "/app/main.py"]"""
-    assert expected_lines in dockerfile_content
+    for line in expected_lines:
+        assert line in dockerfile_content
 
 
 def test_get_dockerfile_with_local_crl():
