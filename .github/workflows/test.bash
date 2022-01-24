adc71a1e11522915a444a33a3626d204fcec4761
M. Mostafa Farzan <farzan@cakerobotics.com>
2022-01-24 05:31:44 +0330

    Simplify CI

---
+++ .github/workflows/test.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+apt-get update
+apt-get install -y \
+    python3-pip \
+    curl
+source /opt/ros/galactic/setup.bash
+# Nasty backport (delete after upgrading to python 3.9)
+curl https://raw.githubusercontent.com/python/cpython/3.9/Lib/asyncio/events.py \
+    --output /usr/lib/python3.8/asyncio/events.py
+pip3 install .
+python3 -m pytest -x -s -v
