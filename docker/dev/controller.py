12ecaaef5c38df9074d387de4bc1f9d48f46eda6
Mohammad Farzan <m2_farzan@yahoo.com>
2022-02-19 16:35:01 +0330

    Use socket for controller

--- docker/dev/controller.py
+++ docker/dev/controller.py
@@ -5,21 +5,24 @@ import pip
 import subprocess
 
 import requests
-# import websockets
+import socketio
 import yaml
 
 
 srcdir = '/app'
-runner = None
+app_process = None
+socket_client = socketio.Client()
 
 
 def main():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
     ensure_workspace_dir()
     ensure_source()
     ensure_requirements()
-    # connect_to_devices_service()
+    connect_to_devices_service()
     start_app()
+    handle_logs()
+    print_exit_code()
 
 
 def ensure_workspace_dir():
@@ -31,11 +34,11 @@ def ensure_source():
     if os.path.isfile(f'{srcdir}/main.py'):
         logging.info('Using mounted source...')
         return
-    logging.info('Fetching source...')
     fetch_source()
 
 
 def fetch_source():
+    logging.info('Fetching source...')
     download_source_file('main.py')
     try:
         download_source_file('props.json')
@@ -43,15 +46,22 @@ def fetch_source():
         download_source_file('props.yaml')
 
 
+def get_auth_header():
+    if os.environ.get('AUTH_HEADER'):
+        return os.environ['AUTH_HEADER']
+    elif os.environ.get('TOKEN'):
+        return f'Device {os.environ["TOKEN"]}'
+    return ''
+
+
 def download_source_file(filename):
-    base_url = os.environ['PROJECT_URL']
-    auth_header = os.environ.get('AUTH_HEADER') or ''
+    base_url = os.environ.get('PROJECT_URL') or f"https://cloud.cakerobotics.com/api/devices/src"
     url = f'{base_url}/{filename}'
-    response = requests.get(url, headers={'AUTHORIZATION': auth_header})
+    headers = {'authorization': get_auth_header()}
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
     with open(f'{srcdir}/{filename}', 'wb') as f:
         f.write(response.content)
-
 
 
 def ensure_requirements():
@@ -74,8 +84,75 @@ def read_requirements_from_props():
     return props.get('pip_requirements') or []
 
 
+def connect_to_devices_service():
+    token = os.environ.get('TOKEN')
+    if not token:
+        logging.info('No device token provided. Skipping socket connection.')
+        return
+    devices_url = 'https://cloud.cakerobotics.com/'
+    socketio_path = '/api/devices/socket'
+    logging.info(f'Connecting to socket at {devices_url}...')
+    socket_client.connect(
+        devices_url,
+        auth={'token': token},
+        socketio_path=socketio_path,
+        wait_timeout=10
+    )
+    logging.info(f'Connected.')
+
+
 def start_app():
-    subprocess.run(['python3', '-u', 'main.py'], cwd='/app')
+    global app_process
+    logging.info('Starting the application...')
+    app_process = subprocess.Popen(
+        ['python3', '-u', 'main.py'],
+        cwd='/app',
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        encoding='utf-8',
+    )
+
+
+def handle_logs():
+    for line in iter(app_process.stdout.readline, ''):
+        print(">>> " + line.rstrip())
+        if socket_client.connected:
+            socket_client.emit('log', line.rstrip())
+
+
+def print_exit_code():
+    logging.info(f'App finished with exit code {app_process.poll()}.')
+
+
+def stop_app():
+    # app_process.send_signal(9)
+    app_process.kill()
+    app_process.wait()
+
+
+@socket_client.on('restart')
+def on_restart():
+    stop_app()
+    fetch_source()
+    ensure_requirements()
+    start_app()
+    handle_logs()
+    print_exit_code()
+
+
+@socket_client.on('stop')
+def on_stop():
+    stop_app()
+
+
+@socket_client.on('error')
+def on_error(msg):
+    logging.error(msg)
+
+
+@socket_client.on('accept')
+def on_establish_ok():
+    logging.info('Socket established.')
 
 
 if __name__ == "__main__":
